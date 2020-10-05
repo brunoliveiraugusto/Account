020dae03d25e52a1db6769e441da2b1bbd747ae3
Bruno Augusto de Souza Oliveira <brunoaugustoliveira@gmail.com>
2020-10-05 17:53:42 -0300

    Sobrescrita do método Withdraw na classe SavingsAccount

---
+++ Account/Account/Entities/SavingsAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account.Entities
+{
+    class SavingsAccount : Account
+    {
+        public override void Withdraw(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
