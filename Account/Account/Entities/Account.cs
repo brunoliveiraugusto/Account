3312ba99816927a147d5a79f0eb79091d69790ee
Bruno Augusto de Souza Oliveira <brunoaugustoliveira@gmail.com>
2020-10-05 17:43:04 -0300

    Criada classe abstrata Account, suas propriedades e método abstrato Withdraw

---
+++ Account/Account/Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account.Entities
+{
+    abstract class Account
+    {
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public int Number { get; set; }
+
+        public abstract void Withdraw(double amount);
+    }
+}
